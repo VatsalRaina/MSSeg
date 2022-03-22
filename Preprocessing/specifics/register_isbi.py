b8d1d3cf320d712f5383b8627c9fe55da830a88e
Vatsal Raina <vatsalraina@hotmail.co.uk>
2022-03-22 21:47:46 +0100

    Average retention curve

--- Preprocessing/specifics/register_isbi.py
+++ Preprocessing/specifics/register_isbi.py
@@ -11,7 +11,8 @@ parser = argparse.ArgumentParser(description='Get all command line arguments.')
 parser.add_argument('--input_flair_proc', type=str, help='Input file location of FLAIR ISBI preprocessed')
 parser.add_argument('--input_flair_raw', type=str, help='Input file location of FLAIR raw')
 parser.add_argument('--input_gt', type=str, help='Input file location of gt in FLAIR ISBI preprocessed space')
-parser.add_argument('--save_file', type=str, help='Where to save output file of nii.gz')
+parser.add_argument('--save_file_temp', type=str, help='Where to save temp output file of nii.gz')
+parser.add_argument('--save_file', type=str, help='Where to save output file of nii')
 
 
 def main(args):
@@ -20,7 +21,11 @@ def main(args):
     mi = ants.image_read(args.input_flair_raw)
     orientation = nib.load(args.input_flair_raw).affine #Save the raw FLAIR orientation
     mytx = ants.registration(fixed=fi, moving=mi, type_of_transform = 'Rigid' ) #RIGID registration of processed FLAIR onto raw
-    
+
+    fl_numpy = mytx['warpedmovout'].numpy()  #Convert the registered image to numpy array
+    nifti_out = nib.Nifti1Image(fl_numpy, affine=orientation)
+    nifti_out.to_filename(args.save_file_temp)
+
     gt_proc = ants.image_read(args.input_gt)
     # Apply rigid transform to gt
     gt_raw = ants.apply_transforms(fixed=fi, moving=gt_proc, transformlist=mytx['fwdtransforms'], interpolator="nearestNeighbor")
