ee8d6c476a282b9e43112167874e75e8521edaad
Vatsal Raina <vatsalraina@hotmail.co.uk>
2022-04-11 18:36:44 +0200

    test script for nfold

---
+++ n_fold/Save_unc_retentionCurve.py
@@ -0,0 +1,260 @@
+"""
+@author: Originally by Francesco La Rosa
+         Adapted by Vatsal Raina
+"""
+
+import argparse
+import os
+import torch
+import sys
+import re
+from glob import glob
+from monai.config import print_config
+from monai.data import CacheDataset, DataLoader, Dataset
+from monai.inferers import sliding_window_inference
+from monai.losses import DiceLoss, GeneralizedDiceLoss, TverskyLoss
+from monai.metrics import compute_meandice, DiceMetric
+from monai.networks.layers import Norm
+from monai.networks.nets import UNet
+from monai.transforms import (
+    AddChanneld,Compose,CropForegroundd,LoadNiftid,Orientationd,RandCropByPosNegLabeld,
+    ScaleIntensityRanged,Spacingd,ToTensord,ConcatItemsd,NormalizeIntensityd, RandFlipd,
+    RandRotate90d,RandShiftIntensityd,RandAffined,RandSpatialCropd, AsDiscrete, Activations)
+from monai.utils import first, set_determinism
+from monai.data import write_nifti, create_file_basename, NiftiDataset
+import numpy as np
+from scipy import ndimage
+
+import matplotlib.pyplot as plt
+import seaborn as sns; sns.set_theme()
+from sklearn import metrics
+
+from joblib import Parallel, delayed
+from functools import partial
+import os
+
+from Uncertainty import ensemble_uncertainties_classification
+
+parser = argparse.ArgumentParser(description='Get all command line arguments.')
+parser.add_argument('--threshold', type=float, default=0.2, help='Threshold for lesion detection')
+parser.add_argument('--num_models', type=int, default=5, help='Number of models in ensemble')
+parser.add_argument('--path_data', type=str, default='', help='Specify the path to the test data files directory')
+parser.add_argument('--path_gts', type=str, default='', help='Specify the path to the test gts directory')
+parser.add_argument('--path_model', type=str, default='', help='Specify the dir to all the trained models')
+parser.add_argument('--num_models', type=int, default=5, help='Number of models in ensemble')
+parser.add_argument('--n_jobs', type=int, default=1, help='Number of cpu cores to use')
+parser.add_argument('--path_save', type=str, default='', help='Save path for numpy array')
+
+
+
+# Set device
+def get_default_device():
+    if torch.cuda.is_available():
+        print("Got CUDA!")
+        return torch.device('cuda')
+    else:
+        return torch.device('cpu')
+
+
+def dice_norm_metric(ground_truth, predictions):
+    """
+    For a single example returns DSC_norm, fpr, fnr
+    """
+
+    # Reference for normalized DSC
+    r = 0.001
+    # Cast to float32 type
+    gt = ground_truth.astype("float32")
+    seg = predictions.astype("float32")
+    im_sum = np.sum(seg) + np.sum(gt)
+    if im_sum == 0:
+        return 1.0, 1.0, 1.0
+    else:
+        if np.sum(gt) == 0:
+            k = 1.0
+        else:
+            k = (1-r) * np.sum(gt) / ( r * ( len(gt.flatten()) - np.sum(gt) ) )
+        tp = np.sum(seg[gt==1])
+        fp = np.sum(seg[gt==0])
+        fn = np.sum(gt[seg==0])
+        fp_scaled = k * fp
+        dsc_norm = 2 * tp / (fp_scaled + 2 * tp + fn)
+
+        fpr = fp / ( len(gt.flatten()) - np.sum(gt) )
+        if np.sum(gt) == 0:
+            fnr = 1.0
+        else:
+            fnr = fn / np.sum(gt)
+        return dsc_norm, fpr, fnr
+
+
+def get_unc_score(gts, preds, uncs, n_jobs):
+
+    def compute_dice_norm(frac_, preds_, gts_, N_):
+        pos = int(N_ * frac_)
+        curr_preds = preds if pos == N_ else np.concatenate((preds_[:pos], gts_[pos:]))
+        return dice_norm_metric(gts_, curr_preds)[0]
+
+    ordering = uncs.argsort()
+    uncs = uncs[ordering]
+    gts = gts[ordering]
+    preds = preds[ordering]
+    N = len(gts)
+    
+    # # Significant class imbalance means it is important to use logspacing between values
+    # # so that it is more granular for the higher retention fractions
+    num_values = 200
+    fracs_retained = np.log(np.arange(num_values+1)[1:])
+    fracs_retained = fracs_retained / np.amax(fracs_retained)
+
+    process = partial(compute_dice_norm, preds_=preds, gts_=gts, N_=N)
+    dsc_norm_scores = np.asarray(
+        Parallel(n_jobs=n_jobs)(delayed(process)(frac) for frac in fracs_retained)
+    )
+
+    return fracs_retained, dsc_norm_scores
+
+
+def main(args):
+
+    if not os.path.isdir('CMDs'):
+        os.mkdir('CMDs')
+    with open('CMDs/train.cmd', 'a') as f:
+        f.write(' '.join(sys.argv) + '\n')
+        f.write('--------------------------------\n')
+
+    # Choose device
+    device = get_default_device()
+
+    path_data = args.path_data  # Path where the data is
+    flair = sorted(glob(os.path.join(path_data, "*FLAIR.nii.gz")),
+                 key=lambda i: int(re.sub('\D', '', i)))  # Collect all flair images sorted
+    segs = sorted(glob(os.path.join(args.path_gts, "*gt.nii")),
+                  key=lambda i: int(re.sub('\D', '', i)))        
+
+
+    N = (len(flair)) 
+
+    indices = np.arange(N)
+    v=indices[:]
+
+    test_files=[]
+    for j in v:
+        test_files = test_files + [{"image": fl, "label": seg} for fl, seg in zip(flair[j:j+1], segs[j:j+1])]
+
+    print("Testing cases:", len(test_files))
+    
+    print()
+    print("-------------------------------------------------------------------")
+    print("Welcome!")
+    print()
+    print('The MS lesion segmentation will be computed on the following files: ')
+    print()
+    print(test_files)
+    print()
+    print("-------------------------------------------------------------------")
+    print()
+    print('Loading the files and the trained network')
+   
+    val_transforms = Compose(
+    [
+        LoadNiftid(keys=["image", "label"]),
+        AddChanneld(keys=["image","label"]),
+        Spacingd(keys=["image", "label"], pixdim=(1.0, 1.0, 1.0), mode=("bilinear", "nearest")),
+        NormalizeIntensityd(keys=["image"], nonzero=True),
+        ToTensord(keys=["image", "label"]),
+    ]
+    )
+    #%%
+
+    val_ds = CacheDataset(data=test_files, transform=val_transforms, cache_rate=0.5, num_workers=0)
+    val_loader = DataLoader(val_ds, batch_size=1, num_workers=0)
+
+    K = args.num_models
+
+    models = []
+    for i in range(K):
+        models.append(UNet(dimensions=3,in_channels=1, out_channels=2,channels=(32, 64, 128, 256, 512),
+                    strides=(2, 2, 2, 2),num_res_units=0).to(device))
+
+    act = Activations(softmax=True)
+
+    for i, model in enumerate(models):
+        model.load_state_dict(torch.load(args.path_model + "seed" + str(i+1) + "/Best_model_finetuning.pth"))
+        model.eval()
+
+    print()
+    print('Running the inference, please wait... ')
+    
+    th = args.threshold
+
+    num_patients = 0
+    all_curves_dsc_norm = []
+
+    with torch.no_grad():
+        for count, batch_data in enumerate(val_loader):
+            num_patients += 1
+            # if count != args.patient_num:
+            #     continue
+            print("Patient num: ", count)
+            inputs, gt  = (
+                    batch_data["image"].to(device),#.unsqueeze(0),
+                     batch_data["label"].type(torch.LongTensor).to(device),)#.unsqueeze(0),)
+            roi_size = (96, 96, 96)
+            sw_batch_size = 4
+
+            val_labels = gt.cpu().numpy()
+            gt = np.squeeze(val_labels)
+
+            all_outputs = []
+            for model in models:
+                outputs = sliding_window_inference(inputs, roi_size, sw_batch_size, model, mode='gaussian')
+                outputs = act(outputs).cpu().numpy()
+                outputs = np.squeeze(outputs[0,1])
+                all_outputs.append(outputs)
+            all_outputs = np.asarray(all_outputs)
+            outputs = np.mean(all_outputs, axis=0)
+
+            # Get all uncertainties
+            uncs = ensemble_uncertainties_classification( np.concatenate( (np.expand_dims(all_outputs, axis=-1), np.expand_dims(1.-all_outputs, axis=-1)), axis=-1) )
+            
+            outputs[outputs>th]=1
+            outputs[outputs<th]=0
+            seg= np.squeeze(outputs)
+
+            """
+            Remove connected components smaller than 10 voxels
+            """
+            l_min = 9
+            labeled_seg, num_labels = ndimage.label(seg)
+            label_list = np.unique(labeled_seg)
+            num_elements_by_lesion = ndimage.labeled_comprehension(seg,labeled_seg,label_list,np.sum,float, 0)
+
+            seg2 = np.zeros_like(seg)
+            for l in range(len(num_elements_by_lesion)):
+                if num_elements_by_lesion[l] > l_min:
+            # assign voxels to output
+                    current_voxels = np.stack(np.where(labeled_seg == l), axis=1)
+                    seg2[current_voxels[:, 0],
+                        current_voxels[:, 1],
+                        current_voxels[:, 2]] = 1
+            seg=np.copy(seg2) 
+
+            # Calculate all AUC-DSCs
+            for unc_key, curr_uncs in uncs.items():
+                if unc_key != "reverse_mutual_information":
+                    continue
+                fracs_retained, dsc_curve, dsc_norm_curve = get_unc_score(gt.flatten(), seg.flatten(), curr_uncs.flatten(), n_jobs=args.n_jobs)
+            all_curves_dsc_norm.append(dsc_norm_curve)
+
+
+    all_curves_dsc_norm3 = np.asarray(all_curves_dsc_norm3)
+    dsc_norm_scores3 = np.mean(all_curves_dsc_norm3, axis=0)
+    
+    print(metrics.auc(fracs_retained, dsc_norm_scores3))
+
+
+#%%
+if __name__ == "__main__":
+    args = parser.parse_args()
+    main(args)
