8ef55ea72d40a34fd3df3e24ffeb98dbcb49fc19
NataliiaMolch <nataliia.molchanova@epfl.ch>
2022-04-26 07:31:41 +0200

    lesion uncs

--- one_modality/Uncertainty.py
+++ one_modality/Uncertainty.py
@@ -3,6 +3,7 @@ Implementation of standard predictive uncertainty measures for image segmentatio
 """
 
 import numpy as np
+from scipy import ndimage
 
 def entropy_of_expected(probs, epsilon=1e-10):
     """
@@ -47,3 +48,25 @@ def ensemble_uncertainties_classification(probs, epsilon=1e-10):
                    }
 
     return uncertainty
+
+
+def lesions_uncertainty_sum(uncs_mask, binary_mask):
+    """
+    Binary mask can be either gt or pred.
+
+    Returns:
+        * list of uncertainties computed as sum of connected component uncertainty
+        in voxels normalized by the number of voxels
+        * connected components map from binary mask
+
+    index in cc map = index in uncertainty list
+
+    index 0 in uncertainty list is background
+    """
+    multi_mask = ndimage.label(binary_mask)        # connected components map
+    uncs_list = []
+    for cc_label in np.unique(multi_mask):
+        cc_mask = (multi_mask == cc_label).astype("float32")
+        cc_unc = np.sum(uncs_mask * cc_mask) / np.sum(cc_mask)
+        uncs_list.append(cc_unc)
+    return uncs_list, multi_mask
