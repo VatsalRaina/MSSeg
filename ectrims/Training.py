7d59fe397dee0acc776586480ef4150477061795
NataliiaMolch <nataliia.molchanova@epfl.ch>
2022-05-04 15:14:13 +0200

    ectrims

---
+++ ectrims/Training.py
@@ -0,0 +1,249 @@
+"""
+@author: Originally by Francesco La Rosa
+         Adapted by Vatsal Raina
+"""
+
+import argparse
+import os
+import sys
+
+import matplotlib.pyplot as plt
+from glob import glob
+import torch
+import re
+from torch import nn
+
+from monai.data import CacheDataset, DataLoader, Dataset
+from monai.inferers import sliding_window_inference
+from monai.losses import DiceLoss, GeneralizedDiceLoss, TverskyLoss
+from monai.metrics import compute_meandice
+from monai.networks.nets import UNet
+from monai.transforms import Activations
+import numpy as np
+import random
+from .data_loader import train_transforms, val_transforms, get_data_loader
+
+
+parser = argparse.ArgumentParser(description='Get all command line arguments.')
+# training
+parser.add_argument('--learning_rate', type=float, default=1e-5, help='Specify the initial learning rate')
+parser.add_argument('--n_epochs', type=int, default=200, help='Specify the number of epochs to train for')
+# model
+parser.add_argument('--seed', type=int, default=1, help='Specify the global random seed')
+parser.add_argument('--threshold', type=float, default=0.4, help='Threshold for lesion detection')
+# data
+parser.add_argument('--path_flair', type=str, default='', help='Specify the path to the flair directory')
+parser.add_argument('--path_mp2rage', type=str, default='', help='Specify the path to the mp2rage directory')
+parser.add_argument('--path_gts', type=str, default='', help='Specify the path to the gts directory')
+parser.add_argument('--flair_prefix', type=str, default="FLAIR.nii.gz", help='name ending FLAIR')
+parser.add_argument('--mp2rage_prefix', type=str, default="UNIT1.nii.gz", help='name ending mp2rage')
+parser.add_argument('--gts_prefix', type=str, default="gt.nii", help='name ending segmentation mask')
+parser.add_argument('--check_dataset', action='store_true',
+                    help='if sent, checks that FLAIR and semg masks names correspond to each other')
+parser.add_argument('--num_workers', type=int, default=0,
+                    help='number of jobs used to load the data, DataLoader parameter')
+# save
+parser.add_argument('--path_save', type=str, default='', help='Specify the path to the save directory')
+
+# Set device
+def get_default_device():
+    if torch.cuda.is_available():
+        print("Got CUDA!")
+        return torch.device('cuda')
+    else:
+        return torch.device('cpu')
+
+
+def main(args):
+    os.makedirs(args.path_save, exist_ok=True)
+
+    seed_val = args.seed
+    random.seed(seed_val)
+    np.random.seed(seed_val)
+    torch.manual_seed(seed_val)
+    torch.cuda.manual_seed_all(seed_val)
+
+    ''' Choose device'''
+    device = get_default_device()
+    val_transforms_seed = val_transforms.set_random_state(seed=seed_val)
+    train_transforms_seed = train_transforms.set_random_state(seed=seed_val)
+
+    ''' Dataloader '''
+    train_loader = get_data_loader(path_flair=args.path_flair,
+                                   path_mp2rage=args.path_mp2rage,
+                                   path_gts=args.path_gts,
+                                   flair_prefix=args.flair_prefix,
+                                   mp2rage_prefix=args.mp2rage_prefix,
+                                   gts_prefix=args.gts_prefix,
+                                   transforms=train_transforms_seed,
+                                   num_workers=args.num_workers,
+                                   batch_size=args.batch_size)
+    val_loader = get_data_loader(path_flair=args.path_flair,
+                                 path_mp2rage=args.path_mp2rage,
+                                 path_gts=args.path_gts,
+                                 flair_prefix=args.flair_prefix,
+                                 mp2rage_prefix=args.mp2rage_prefix,
+                                 gts_prefix=args.gts_prefix,
+                                 transforms=val_transforms_seed,
+                                 num_workers=args.num_workers,
+                                 batch_size=args.batch_size)
+    val_train_loader = get_data_loader(path_flair=args.path_flair,
+                                       path_mp2rage=args.path_mp2rage,
+                                       path_gts=args.path_gts,
+                                       flair_prefix=args.flair_prefix,
+                                       mp2rage_prefix=args.mp2rage_prefix,
+                                       gts_prefix=args.gts_prefix,
+                                       transforms=val_transforms_seed,
+                                       num_workers=args.num_workers,
+                                       batch_size=args.batch_size)
+
+    ''' Init model '''
+    model = UNet(
+        dimensions=3,
+        in_channels=2,
+        out_channels=2,
+        channels=(32, 64, 128, 256, 512),
+        strides=(2, 2, 2, 2),
+        num_res_units=0).to(device)
+    loss_function = DiceLoss(to_onehot_y=True, softmax=True,
+                             sigmoid=False,
+                             include_background=False)
+
+    optimizer = torch.optim.Adam(model.parameters(), args.learning_rate)
+    # Load trained model
+    # model.load_state_dict(torch.load(os.path.join(root_dir, "Initial_model.pth")))
+
+    epoch_num = args.n_epochs
+    val_interval = 5
+    best_metric = -1
+    best_metric_epoch = -1
+    epoch_loss_values = list()
+    metric_values = list()
+    metric_values_train = list()
+    act = Activations(softmax=True)
+    thresh = args.threshold
+
+    for epoch in range(epoch_num):
+        print("-" * 10)
+        print(f"epoch {epoch + 1}/{epoch_num}")
+        model.train()
+        epoch_loss = 0
+        step = 0
+        for batch_data in train_loader:
+            n_samples = batch_data["image"].size(0)
+            for m in range(0, batch_data["image"].size(0), 2):
+                step += 2
+                inputs, labels = (
+                    batch_data["image"][m:(m + 2)].to(device),
+                    batch_data["label"][m:(m + 2)].type(torch.LongTensor).to(device))
+                optimizer.zero_grad()
+                outputs = model(inputs)
+
+                # Dice loss
+                loss1 = loss_function(outputs, labels)
+
+                # Focal loss
+                ce_loss = nn.CrossEntropyLoss(reduction='none')
+                ce = ce_loss(outputs, torch.squeeze(labels, dim=1))
+                gamma = 2.0
+                pt = torch.exp(-ce)
+                f_loss = 1 * (1 - pt) ** gamma * ce
+                loss2 = f_loss
+                loss2 = torch.mean(loss2)
+                loss = 0.5 * loss1 + loss2
+
+                loss.backward()
+                optimizer.step()
+
+                epoch_loss += loss.item()
+                if step % 100 == 0:
+                    step_print = int(step / 2)
+                    print(
+                        f"{step_print}/{(len(train_loader) * n_samples) // (train_loader.batch_size * 2)}, train_loss: {loss.item():.4f}")
+
+        epoch_loss /= step_print
+        epoch_loss_values.append(epoch_loss)
+        print(f"epoch {epoch + 1} average loss: {epoch_loss:.4f}")
+
+        if (epoch + 1) % val_interval == 0:
+            model.eval()
+            with torch.no_grad():
+                metric_sum = 0.0
+                metric_count = 0
+                for val_data in val_loader:
+                    val_inputs, val_labels = (
+                        val_data["image"].to(device),
+                        val_data["label"].to(device),
+                    )
+                    roi_size = (96, 96, 96)
+                    sw_batch_size = 4
+                    val_outputs = sliding_window_inference(val_inputs, roi_size, sw_batch_size, model, mode='gaussian')
+
+                    val_labels = val_labels.cpu().numpy()
+                    gt = np.squeeze(val_labels)
+                    val_outputs = act(val_outputs).cpu().numpy()
+                    seg = np.squeeze(val_outputs[0, 1])
+                    seg[seg > thresh] = 1
+                    seg[seg < thresh] = 0
+                    value = (np.sum(seg[gt == 1]) * 2.0) / (np.sum(seg) + np.sum(gt))
+
+                    metric_count += 1
+                    metric_sum += value.sum().item()
+                metric = metric_sum / metric_count
+                metric_values.append(metric)
+                metric_sum_train = 0.0
+                metric_count_train = 0
+                # for train_data in val_train_loader:
+                #     train_inputs, train_labels = (
+                #         train_data["image"].to(device),
+                #         train_data["label"].to(device),
+                #     )
+                #     roi_size = (96, 96, 96)
+                #     sw_batch_size = 4
+                #     train_outputs = sliding_window_inference(train_inputs, roi_size, sw_batch_size, model,
+                #                                              mode='gaussian')
+                #
+                #     train_labels = train_labels.cpu().numpy()
+                #     gt = np.squeeze(train_labels)
+                #     train_outputs = act(train_outputs).cpu().numpy()
+                #     seg = np.squeeze(train_outputs[0, 1])
+                #     seg[seg > thresh] = 1
+                #     seg[seg < thresh] = 0
+                #     value_train = (np.sum(seg[gt == 1]) * 2.0) / (np.sum(seg) + np.sum(gt))
+                #
+                #     metric_count_train += 1
+                #     metric_sum_train += value_train.sum().item()
+                # metric_train = metric_sum_train / metric_count_train
+                # metric_values_train.append(metric_train)
+                if metric > best_metric:
+                    best_metric = metric
+                    best_metric_epoch = epoch + 1
+                    torch.save(model.state_dict(), os.path.join(args.path_save, "Best_model_finetuning.pth"))
+                    print("saved new best metric model")
+                print(f"current epoch: {epoch + 1} current mean dice: {metric:.4f}"
+                      f"\nbest mean dice: {best_metric:.4f} at epoch: {best_metric_epoch}"
+                      )
+                plt.figure("train", (12, 6))
+                plt.subplot(1, 2, 1)
+                plt.title("Epoch Average Train Loss")
+                x = [i + 1 for i in range(len(epoch_loss_values))]
+                y = epoch_loss_values
+                plt.xlabel("epoch")
+                plt.plot(x, y)
+                plt.subplot(1, 2, 2)
+                plt.title("Val and Train Mean Dice")
+                x = [val_interval * (i + 1) for i in range(len(metric_values))]
+                y = metric_values
+                y1 = metric_values_train
+                plt.xlabel("epoch")
+                plt.plot(x, y)
+                plt.plot(x, y1)
+                plt.savefig(os.path.join(args.path_save, 'train_history.png'))
+                plt.show()
+
+            # %%
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    main(args)
---
+++ ectrims/data_loader.py
@@ -0,0 +1,95 @@
+from monai.data import CacheDataset, DataLoader, Dataset
+from monai.inferers import sliding_window_inference
+from monai.losses import DiceLoss, GeneralizedDiceLoss, TverskyLoss
+from monai.metrics import compute_meandice
+from monai.networks.nets import UNet
+from monai.transforms import (
+    AddChanneld,Compose,CropForegroundd,LoadNiftid,Orientationd,RandCropByPosNegLabeld,
+    ScaleIntensityRanged,Spacingd,ToTensord,ConcatItemsd,NormalizeIntensityd, RandFlipd,
+    RandRotate90d,RandShiftIntensityd,RandAffined,RandSpatialCropd, RandScaleIntensityd, Activations,SqueezeDimd)
+import numpy as np
+import os
+from glob import glob
+import re
+
+train_transforms = Compose(
+    [
+        LoadNiftid(keys=["flair", "mp2rage" ,"label"]),
+
+        # SqueezeDimd(keys=["flair", "mp2rage"], dim=-1),
+
+        AddChanneld(keys=["flair", "mp2rage" ,"label"]),
+        Spacingd(keys=["flair" ,"mp2rage" ,"label"], pixdim=(1.0, 1.0, 1.0), mode=("bilinear", "bilinear" ,"nearest")),
+        NormalizeIntensityd(keys=["flair", "mp2rage"], nonzero=True),
+        RandShiftIntensityd(keys="flair" ,offsets=0.1 ,prob=1.0),
+        RandShiftIntensityd(keys="mp2rage" ,offsets=0.1 ,prob=1.0),
+        RandScaleIntensityd(keys="flair" ,factors=0.1 ,prob=1.0),
+        RandScaleIntensityd(keys="mp2rage" ,factors=0.1 ,prob=1.0),
+        ConcatItemsd(keys=["flair", "mp2rage"], name="image"),
+        RandCropByPosNegLabeld(keys=["image", "label"] ,label_key="label" ,spatial_size=(128, 128, 128),
+                               pos=4 ,neg=1 ,num_samples=32 ,image_key="image"),
+        RandSpatialCropd(keys=["image", "label"], roi_size=(96 ,96 ,96), random_center=True, random_size=False),
+        RandFlipd (keys=["image", "label"] ,prob=0.5 ,spatial_axis=(0 ,1 ,2)),
+        RandRotate90d (keys=["image", "label"] ,prob=0.5 ,spatial_axes=(0 ,1)),
+        RandRotate90d (keys=["image", "label"] ,prob=0.5 ,spatial_axes=(1 ,2)),
+        RandRotate90d (keys=["image", "label"] ,prob=0.5 ,spatial_axes=(0 ,2)),
+        RandAffined(keys=['image', 'label'], mode=('bilinear', 'nearest'), prob=1.0, spatial_size=(96, 96, 96),
+                    rotate_range=(np.pi /12, np.pi /12, np.pi /12), scale_range=(0.1, 0.1, 0.1)
+                    ,padding_mode='border'),
+        ToTensord(keys=["image", "label"]),
+    ]
+)
+val_transforms = Compose(
+    [
+        LoadNiftid(keys=["flair", "mp2rage", "label"]),
+        # SqueezeDimd(keys=["flair", "mp2rage"], dim=-1),
+        AddChanneld(keys=["flair", "mp2rage" ,"label"]),
+        Spacingd(keys=["flair", "mp2rage" ,"label"], pixdim=(1.0, 1.0, 1.0), mode=("bilinear", "bilinear" ,"nearest")),
+        NormalizeIntensityd(keys=["flair", "mp2rage"], nonzero=True),
+        ConcatItemsd(keys=["flair", "mp2rage"], name="image"),
+        ToTensord(keys=["image", "label"]),
+    ]
+)
+
+
+def check_dataset(filepaths_list, prefixes):
+    """Check that there are equal amounts of files in both lists and
+    the names before prefices are similar for each of the matched pairs of
+    flair image filepath and gts filepath.
+    Parameters:
+        - flair_filepaths (list) - list of paths to flair images
+        - gts_filepaths (list) - list of paths to lesion masks of flair images
+        - args (argparse.ArgumentParser) - system arguments, should contain `flair_prefix` and `gts_prefix`
+    """
+    if len(filepaths_list) > 1:
+        filepaths_ref = filepaths_list[0]
+        for filepaths in filepaths_list[1:]:
+            assert len(filepaths_ref) == len(
+                filepaths), f"Found {len(filepaths_ref)} ref files and {len(filepaths)}"
+        for sub_filepaths in list(map(list, zip(*filepaths_list))):
+            filepath_ref = sub_filepaths[0]
+            prefix_ref = prefixes[0]
+            for filepath, prefix in zip(sub_filepaths[1:], prefixes[1:]):
+                if os.path.basename(filepaths_ref)[:-len(prefix_ref)] != os.path.basename(filepath)[:-len(prefix)]:
+                    raise ValueError(f"{filepath_ref} and {filepath} do not match")
+
+
+def get_data_loader(path_flair, path_mp2rage, path_gts, flair_prefix, mp2rage_prefix, gts_prefix, check=True,
+                    transforms=None, num_workers=0, batch_size=1):
+    """ Create a torch DataLoader based on the system arguments.
+    """
+    flair = sorted(glob(os.path.join(path_flair, f"*{flair_prefix}")),
+                   key=lambda i: int(re.sub('\D', '', i)))  # Collect all flair images sorted
+    mp2rage = sorted(glob(os.path.join(path_mp2rage, f"*{mp2rage_prefix}")),
+                   key=lambda i: int(re.sub('\D', '', i)))
+    segs = sorted(glob(os.path.join(path_gts, f"*{gts_prefix}")),
+                  key=lambda i: int(re.sub('\D', '', i)))
+    if check:
+        check_dataset([flair, mp2rage, segs], [flair_prefix, mp2rage_prefix, gts_prefix])
+
+    print(f"Initializing the dataset. Number of subjects {len(flair)}")
+
+    files = [{"flair": fl, "mp2rage": mp2, "label": seg} for fl, mp2, seg in zip(flair, mp2rage, segs)]
+
+    val_ds = CacheDataset(data=files, transform=transforms, cache_rate=0.5, num_workers=num_workers)
+    return DataLoader(val_ds, batch_size=batch_size, num_workers=num_workers)
