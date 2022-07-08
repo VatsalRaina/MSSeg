af6345059fce2e5adf653e87a67c54684197b822
Vatsal Raina <vatsalraina@hotmail.co.uk>
2022-07-08 12:51:26 +0100

    conversion for gc

---
+++ nifty_to_mha.py
@@ -0,0 +1,26 @@
+"""
+Save .nii images as equivalent .mha files (necessary for GranChallenge)
+"""
+import argparse
+import SimpleITK as sitk
+
+parser = argparse.ArgumentParser(description='Get all command line arguments.')
+parser.add_argument('--in_file', type=str, default=None, help='.nii file input')
+parser.add_argument('--out_file', type=str, default=None, help='.mha file to be produced')
+
+
+def main(args):
+
+    reader = sitk.ImageFileReader()
+    reader.SetImageIO("NiftiImageIO")
+    reader.SetFileName(args.in_file)
+    image = reader.Execute()
+
+    writer = sitk.ImageFileWriter()
+    writer.SetFileName(args.out_file)
+    writer.Execute(image)
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    main(args)
